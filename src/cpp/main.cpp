e9b077f2be71e38f4be4f2565fed855f0a59bfdf
Azizbek1308 <138809274+Azizbek1308@users.noreply.github.com>
2024-01-23 23:40:33 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,18 +6,13 @@
  * Name:
  */
 
-#include <iostream>
+/*#include <iostream>
 #include "task_1.cpp"
 #include "task_2.cpp"
 #include "task_3.cpp"
 #include "task_4.cpp"
 #include "task_5.cpp"
 #include "task_6.cpp"
+#include "task_7.cpp"
+#include "task_8.cpp"*/
 
-using namespace std;
-
-int main() {
-    // write your code here
-
-    return 0;
-}
